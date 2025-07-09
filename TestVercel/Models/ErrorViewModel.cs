2deb63cc4a7bd3bd16acbe2dd40be4f5fc158f66
ShubhanshJaiswal <codingbrains36@gmail.com>
2025-07-09 19:18:51 +0530

    Add project files.

---
+++ TestVercel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestVercel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
